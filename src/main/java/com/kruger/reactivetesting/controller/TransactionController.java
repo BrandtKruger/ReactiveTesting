719e1812be248c7047ddb097842deaf44d146f73
Brandt Kruger <brandt.kruger087@gmial.com>
2020-02-27 20:50:26 +0200

    Add Endpoints.

--- src/main/java/com/kruger/reactivetesting/controller/TransactionController.java
+++ src/main/java/com/kruger/reactivetesting/controller/TransactionController.java
@@ -6,10 +6,15 @@ import io.r2dbc.spi.ConnectionFactory;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
+import org.reactivestreams.Publisher;
 import org.springframework.data.r2dbc.core.DatabaseClient;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Flux;
+
+import java.net.URI;
+import java.util.concurrent.Flow;
 
 @RestController
 @Data
@@ -26,7 +31,41 @@ public class TransactionController {
     }
 
     @GetMapping("/")
-    public Flux<Transaction> findAll(){
-        return transactionRepository.findAll();
+    Publisher<Transaction> findAll(){
+        return this.transactionRepository.findAll();
+    }
+    @GetMapping("/cardNumber")
+    Publisher<Transaction> getByCardNumber(@PathVariable("cardNumber") String cardNumber){
+        return this.transactionRepository.getTransactionsByCardNumber(cardNumber);
+    }
+
+    @GetMapping("/{merchantId}")
+    Publisher<Transaction> getByMerchantId(@PathVariable("merchantId") String merchantId){
+        return this.transactionRepository.getTransactionByMerchantId(merchantId);
+    }
+
+    @GetMapping("/{terminalId}")
+    Publisher<Transaction> getByTerminalId(@PathVariable("terminalId") String terminalId){
+        return this.transactionRepository.getTransactionByTerminalId(terminalId);
+    }
+
+    @GetMapping("/{posId}/{operatorId}")
+    Publisher<Transaction> getByPosIdAndOperatorId(@PathVariable("posId") String posId,
+                                                   @PathVariable("operatorId") String operatorId){
+        return this.transactionRepository.getTransactionByPosIdAndOperatorId(posId, operatorId);
+    }
+
+    @PostMapping
+    Publisher<ResponseEntity<Transaction>> create(@RequestBody Transaction transaction){
+        return this.transactionRepository
+                .save(transaction)
+                .map( tr -> ResponseEntity.created(URI.create("/transactions/" + tr.getTransactionId())).
+                        contentType(MediaType.APPLICATION_JSON)
+                        .build());
+    }
+
+    @DeleteMapping("/delete/{transactionId}")
+    Publisher<Transaction> deleteByTransactionId(@PathVariable("transactionId") Long transactionId){
+        return this.transactionRepository.deleteById(transactionId);
     }
 }
